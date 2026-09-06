491b7f9e412218d787522a82de19447e26e8e04a
SiptiG <cra>
2026-09-06 19:57:25 +0330

    Initial project upload

--- backend/src/routes/contact.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { contactRateLimiter } from "../middleware/rateLimiter.js";
-import { submitContactForm } from "../controllers/contact.controller.js";
-
-const router = Router();
-
-router.post("/", contactRateLimiter, submitContactForm);
-
-export default router;
